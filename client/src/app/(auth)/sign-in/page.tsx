9414e88cb9934000eec00d37705ac81d548a37a5
Subham <97989643+Subham-Maity@users.noreply.github.com>
2024-11-25 02:46:07 +0530

    feat(server): prisma setup backend

--- client/src/app/(auth)/sign-in/page.tsx
+++ client/src/app/(auth)/sign-in/page.tsx
@@ -1,11 +1,11 @@
 "use client";
 import React from "react";
-import SignInButton from "@/components/auth/button/task/sign-in-button";
+import SignInButton from "@/components/auth/sign-in-button";
 
 const SignInPage = () => {
   return (
-    <div className="flex flex-col items-center justify-center bg-[#171514] w-full min-h-screen py-16">
-      <div className="absolute top-1/2 left-1/2 -translate-x-1/2 -translate-y-1/2">
+    <div className="flex min-h-screen w-full flex-col items-center justify-center bg-[#171514] py-16">
+      <div className="absolute left-1/2 top-1/2 -translate-x-1/2 -translate-y-1/2">
         <SignInButton />
       </div>
     </div>
